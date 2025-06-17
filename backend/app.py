507813aa5d1b088ffe337764efb8d752435cacff
Rohit Patil <rohit.patil01@sap.com>
2025-06-17 20:17:35 +0530

    adding port

--- backend/app.py
+++ backend/app.py
@@ -164,5 +164,7 @@ def execute_sql(query: str):
         print("Error executing query:", e)
         return {'error': str(e)}
 
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
